f7d44ce933b5fce8938bb73a42bd0ed9c4908080
Nikhil Pinto <nikhil.pinto@quantiphi.com>
2020-03-17 14:21:26 +0530

    solved missing number, restructured folder structure

---
+++ algorithms/search/missingNumbersProblem.java
@@ -0,0 +1,64 @@
+/**
+ * Missing Numbers
+ * Java 8
+ * Problem on HackerRank: https://www.hackerrank.com/challenges/missing-numbers/problem
+ * 
+ * Time Complexity: O(n) where n is the size of brr which is the list with all numbers.
+ * Space Complexity: O(n) where n is the size of brr which is the list with all numbers.
+ * Note: We do not count the space for frequency arrays as they do not change with the size of the input
+ *       & hence is constant space.
+ * 
+ * Approach:
+ * The problem states that there are some numbers missing in arr. Since brr is a permutation of arr that means
+ * all values will exist in brr. Hence we need to keep track of the frequency of numbers in arr & brr. Once we 
+ * have the frequencies, all we need to do is compare them and if the frequency is brr is more than arr, we have 
+ * found our missing number. Arrays are a great way to track frequencies of each number. Its given in the problem
+ * that the difference between the max and min numbers is less than 100. We can use this as a offset for our array
+ * so that we can keep the size small (only 101) rather than the max value of the number in brr.
+ */
+
+import java.util.*;
+
+public class missingNumbersProblem {
+
+    /**
+     * Returns the missing numbers in arr which
+     * @param arr The list which has missing numbers
+     * @param brr The list which has all the numbers 
+     * @return An array of missing numbers in ascending order
+     */
+    static int[] missingNumbers(int[] arr, int[] brr) {
+        int[] freqArr = new int[101];
+        int[] freqBrr = new int[101];
+        int minVal = 10000;
+        for (int i = 0; i < brr.length; i++) {
+            if  (brr[i] < minVal) minVal = brr[i];
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int curVal = arr[i];
+            freqArr[curVal-minVal] += 1;
+        }
+        for (int i = 0; i < brr.length; i++) {
+            int curVal = brr[i];
+            freqBrr[curVal-minVal] += 1;
+        }
+        List<Integer> missingNumbersList = new ArrayList<Integer>();
+        for (int i = 0; i < 101; i++) {
+            if (freqBrr[i] > freqArr[i]) {
+                missingNumbersList.add(i+minVal);
+            }
+        }
+        int[] res = missingNumbersList.stream().mapToInt(Integer::intValue).toArray();
+        return res;     
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {203, 204, 205, 206, 207, 208, 203, 204, 205, 206};
+        int [] brr = {203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204};
+
+        int[] result = missingNumbers(arr, brr);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]+" ");
+        }
+    }
+}
---
+++ algorithms/sorting/fullCountingSort.java
@@ -0,0 +1,53 @@
+
+/**
+ * The Full Counting Sort
+ * Java 8
+ * Problem on HackerRank - https://www.hackerrank.com/challenges/countingsort4/problem
+ * 
+ * Time Complexity: O(n) where n is the size of the number of integer/string pairs in the array.
+ * Space Complexity: O(n) where n is the size of the number of integer/string pairs in the array.
+ * 
+ * Approach:
+ * Although the problem requires you to use counting sort, a better way is to use a HashMap
+ * to maintain the integer <--> strings mapping. In this way we are able to get a stable sort since
+ * the strings are appended to the map in the order they are taken from the input. Once the Map is created
+ * we can simply iterate through it in ascending order and print the result.
+ * Note: I have used string builder specifically and directly in the main function, as using the pre generated
+ * code would timeout in the test cases even though my solution was correct. Using System.out.println also times
+ * out in the test cases.
+ */
+
+import java.io.*;
+import java.util.*;
+
+class fullCountingSort {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+        HashMap<Integer, StringBuilder> hashMap = new HashMap<Integer, StringBuilder>();
+        for (int i = 0; i < n; i++) {
+            String[] tmp = in.readLine().split(" ");
+            int value = Integer.parseInt(tmp[0]);
+            String strVal = tmp[1];
+            if (i < n / 2) {
+                strVal = "-";
+            }
+            if (hashMap.containsKey(value)) {
+                StringBuilder tempBuilder = hashMap.get(value);
+                tempBuilder.append(strVal + " ");
+                hashMap.put(value, tempBuilder);
+            } else {
+                StringBuilder str = new StringBuilder(strVal + " ");
+                hashMap.put(value, str);
+            }
+        }
+
+        StringBuilder str = new StringBuilder();
+        hashMap.entrySet().forEach(entry -> {
+            StringBuilder currString = entry.getValue();
+            str.append(currString.toString());
+        });
+        System.out.print(str.toString());
+        in.close();
+    }
+}
